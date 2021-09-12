e86d7eabcccd6a36c16f2c1db297140e012795cc
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2021-09-11 22:19:06 -0300

    feat: step connector styled component

---
+++ src/components/StepConnectorStyled/index.js
@@ -0,0 +1,31 @@
+// Libs
+import StepConnector from '@material-ui/core/StepConnector';
+
+// Styles
+import { withStyles } from '@material-ui/core/styles';
+
+const StepConnectorStyled = withStyles({
+  alternativeLabel: {
+    top: 22,
+  },
+  active: {
+    '& $line': {
+      backgroundImage:
+        'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
+    },
+  },
+  completed: {
+    '& $line': {
+      backgroundImage:
+        'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
+    },
+  },
+  line: {
+    height: 3,
+    border: 0,
+    backgroundColor: '#eaeaf0',
+    borderRadius: 1,
+  },
+})(StepConnector);
+
+export default StepConnectorStyled
