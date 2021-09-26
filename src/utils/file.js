b8f2ea80f3e34d00a6a5ee960fb5b26ff0139253
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2021-09-26 10:55:22 -0300

    feat: convertBase64IntoImg functionality which results in a hidden additional element

--- src/utils/file.js
+++ src/utils/file.js
@@ -1,3 +1,16 @@
+// Utils
+import _isEmpty from 'lodash/isEmpty'
+
+const convertBase64IntoImg = (base64Data) => {
+  if (_isEmpty(base64Data)) return base64Data
+  const img = new Image()
+  img.src = base64Data
+  img.style = 'visibility: hidden;'
+  document.body.appendChild(img)
+  return img
+}
+
+
 const fileToDataURL = (file) => {
   var reader = new FileReader()
   return new Promise(function (resolve) {
@@ -8,4 +21,4 @@ const fileToDataURL = (file) => {
   })
 }
 
-export { fileToDataURL }
+export { convertBase64IntoImg, fileToDataURL }
