10b5b05d9efa7951f209e7e368e9c1b0ea795993
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2021-09-27 23:38:31 -0300

    feat: restyling the including informations step and including additional functionalities

--- src/components/IncludingInformations/index.js
+++ src/components/IncludingInformations/index.js
@@ -1,59 +1,129 @@
 // Libs
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 // Components
+import Button from '@mui/material/Button'
 import Grid from '@mui/material/Grid'
+import InputAdornment from '@mui/material/InputAdornment'
 import TextField from '@mui/material/TextField'
+
+// Icons
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney'
+import LabelIcon from '@mui/icons-material/Label'
 
 // Styles
 import './index.css'
 
-// Utils
-import _isEmpty from 'lodash/isEmpty'
-
 function IncludingInformations({ pictures, picturesInfo, setPicturesInfo }) {
-  const [actualPictureIndex, setActualPictureIndex] = useState(0)
+  const [actualImgIndex, setActualImgIndex] = useState(0)
+  const [idValue, setIdValue] = useState(picturesInfo[actualImgIndex].id)
+  const [amountValue, setAmountValue] = useState(picturesInfo[actualImgIndex].value)
+  const isFirstPicture = actualImgIndex <= 0
+  const isLastPicture = actualImgIndex >= pictures?.length - 1
+  
+  const resetValues = (indexTarget) => {
+    setIdValue(picturesInfo[indexTarget].id)
+    setAmountValue(picturesInfo[indexTarget].value)
+  }
 
-  console.log('picturesinfo = ', picturesInfo)
+  const backImg = () => {
+    if (isFirstPicture) return false
+    const indexTarget = actualImgIndex - 1
+    setActualImgIndex(indexTarget)
+    resetValues(actualImgIndex - 1)
+  }
 
-  const updateID = (e) => {
-    const valueMapped = e?.target?.value
-    if (_isEmpty(valueMapped)) return false;
+  const nextImg = () => {
+    if (isLastPicture) return false
+    const indexTarget = actualImgIndex + 1
+    setActualImgIndex(indexTarget)
+    resetValues(indexTarget)
+  }
+
+  const updatePicturesInfo = () => {
     let picturesInfoMapped = picturesInfo
-    picturesInfoMapped[actualPictureIndex].value = valueMapped
-    console.log(picturesInfoMapped)
+    picturesInfoMapped[actualImgIndex].id = idValue
+    picturesInfoMapped[actualImgIndex].value = amountValue
     setPicturesInfo(picturesInfoMapped)
   }
 
-  const updateValue = () => {
-
-  }
+  useEffect(() => {
+    updatePicturesInfo()
+  }, [idValue, amountValue])
 
   return (
     <div className="includingInfoContainer">
-      <Grid container>
-        <Grid item xs={12}>
-          <img src={pictures[actualPictureIndex]} className="infoImg" alt='Img info' />
-        </Grid>
-        <div className="imgInfo">
+      <div className="infoContainer">
+        <Grid container>
           <Grid item xs={12}>
-            <TextField
-              className="infoInput"
-              required
-              label="Identificador do produto"
-              onBlur={updateID}
-            />
+            <img src={pictures[actualImgIndex]} className="infoImg" alt='Img info' />
           </Grid>
-          <Grid item xs={12} className="valueContainer">
-            <TextField
-              className="infoInput"
-              required
-              label="Valor (R$)"
-              onBlur={updateValue}
-            />
-          </Grid>
-        </div>
-      </Grid>
+          <div className="actionsContainer">
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={6}>
+                <Button
+                  disabled={isFirstPicture}
+                  variant="contained"
+                  startIcon={<ArrowBackIcon />}
+                  color="error"
+                  onClick={backImg}
+                >
+                  Anterior
+                </Button>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Button
+                  disabled={isLastPicture}
+                  variant="contained"
+                  endIcon={<ArrowForwardIcon />}
+                  color="secondary"
+                  onClick={nextImg}
+                >
+                  Próxima
+                </Button>
+              </Grid>
+            </Grid>
+          </div>
+          <div className="imgInfo">
+            <Grid item xs={12}>
+              <TextField
+                className="infoInput"
+                required
+                label="Identificador do produto"
+                onChange={(e) => setIdValue(e.target.value)}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="end">
+                      <LabelIcon color="primary" />
+                    </InputAdornment>
+                  )
+                }}
+                placeholder="Identificador do produto"
+                value={idValue}
+              />
+            </Grid>
+            <Grid item xs={12} className="valueContainer">
+              <TextField
+                className="infoInput"
+                required
+                label="Valor do produto(R$)"
+                onChange={(e) => setAmountValue(e.target.value)}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="end">
+                      <AttachMoneyIcon className="moneyIcon" />
+                    </InputAdornment>
+                  )
+                }}
+                placeholder="Valor do produto(R$)"
+                value={amountValue}
+              />
+            </Grid>
+          </div>
+        </Grid>
+      </div>
     </div>
   )
 }
