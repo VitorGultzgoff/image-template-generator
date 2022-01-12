e3b6d7a9f3ed0651ef8554f171fcb3332b3cf530
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2022-01-12 00:44:46 -0300

    refactor: replacing the mirror cropped picture logic for the select image step

--- src/components/SelectImages/index.js
+++ src/components/SelectImages/index.js
@@ -1,66 +1,57 @@
 // Libs
-import React from 'react'
-import { FilePond, registerPlugin } from 'react-filepond'
+import React from "react";
+import { FilePond, registerPlugin } from "react-filepond";
 
 // Utils
-import _clone from 'lodash/clone'
-import _isNil from 'lodash/isNil'
-import { fileToDataURL } from 'utils/file'
+import { fileToDataURL } from "utils/file";
 
 // Styles
-import './index.css'
+import "./index.css";
 
 // FilePond
-import 'filepond/dist/filepond.min.css'
-import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation'
-import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
-import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css'
-registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview)
+import "filepond/dist/filepond.min.css";
+import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
 
-function SelectImages({ croppedPictures, pictures, setCroppedPictures, setPictures, setPicturesInfo }) {
-  const mapInitialCroppedPictures = (pictures) => {
-    const basicPictures = _clone(pictures)
-    const mappedCroppedPictures = _clone(croppedPictures)
-    basicPictures.forEach((actualBasicPicture, actualBasicPictureIndex) => {
-      if (_isNil(mappedCroppedPictures[actualBasicPictureIndex])) {
-        mappedCroppedPictures[actualBasicPictureIndex] = actualBasicPicture
-      }
-    });
-    setCroppedPictures(mappedCroppedPictures)
-  }
-
+function SelectImages({
+  pictures,
+  setCroppedPictures,
+  setPictures,
+  setPicturesInfo,
+}) {
 
   const setMappedPicturesInfo = (imgsData) => {
-    const picturesInfo = []
+    const picturesInfo = [];
     // May be replaced for an array create without forEach
-    imgsData.forEach(actualImg => {
+    imgsData.forEach((actualImg) => {
       picturesInfo.push({
-        id: '',
-        value: ''
-      })
+        id: "",
+        value: "",
+      });
     });
-    setPicturesInfo(picturesInfo)
-  }
+    setPicturesInfo(picturesInfo);
+  };
 
+  const transformFilesIntoImages = (files) => {
+    const filesArray = Array.prototype.slice.call(files);
+    return Promise.all(filesArray.map(fileToDataURL));
+  };
 
-  const transformFilesIntoImages = files => {
-    const filesArray = Array.prototype.slice.call(files)
-    return Promise.all(filesArray.map(fileToDataURL))
-  }
+  const transformPondDataIntoImages = (pondData) => {
+    return pondData.map((actualPondData) => actualPondData.file);
+  };
 
-  const transformPondDataIntoImages = pondData => {
-    return pondData.map(actualPondData => actualPondData.file)
-  }
-
-  const uploadImages = (pondData) => {
-    const files = transformPondDataIntoImages(pondData)
-    const images = transformFilesIntoImages(files)
-    images.then(imgsData => {
-      setPictures(imgsData)
-      mapInitialCroppedPictures(imgsData)
-      setMappedPicturesInfo(imgsData)
-    })
-  }
+  const uploadImages = (pondData, quantityOfImagesUploaded) => {
+    const files = transformPondDataIntoImages(pondData);
+    const images = transformFilesIntoImages(files);
+    images.then((imgsData) => {
+      setPictures(imgsData);
+      setCroppedPictures(imgsData)
+      setMappedPicturesInfo(imgsData);
+    });
+  };
 
   return (
     <FilePond
@@ -72,7 +63,7 @@ function SelectImages({ croppedPictures, pictures, setCroppedPictures, setPictur
       name="files"
       labelIdle='Arraste e solte seus arquivos ou <span class="filepond--label-action">Procure o arquivo</span>'
     />
-  )
+  );
 }
 
-export default SelectImages
+export default SelectImages;
