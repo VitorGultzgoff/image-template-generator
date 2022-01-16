8abaa704e0331a463f4891825d050f721ba3f59f
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2022-01-16 00:12:33 -0300

    feat: switch root styled component definition

---
+++ src/components/form/Switch/SwitchRoot/index.js
@@ -0,0 +1,11 @@
+import { styled } from "@mui/system";
+
+const SwitchRoot = styled("input")`
+  display: inline-block;
+  position: relative;
+  width: 64px;
+  height: 36px;
+  padding: 8px;
+`;
+
+export default SwitchRoot;
