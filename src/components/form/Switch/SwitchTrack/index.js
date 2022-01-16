c90fb12f70fe41bf2b8f51a088cdfbb791fcfec1
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2022-01-16 00:14:42 -0300

    feat: switch track styled component definition

---
+++ src/components/form/Switch/SwitchTrack/index.js
@@ -0,0 +1,11 @@
+import { styled } from "@mui/system";
+
+const SwitchTrack = styled("input")`
+  background-color: #bfc7cf;
+  border-radius: 4px;
+  width: 100%;
+  height: 100%;
+  display: block;
+`;
+
+export default SwitchTrack;
