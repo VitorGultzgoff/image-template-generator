d72743a5615e937746566ac988d63020b3d526c2
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2022-01-16 00:12:38 -0300

    feat: switch input styled component definition

---
+++ src/components/form/Switch/SwitchInput/index.js
@@ -0,0 +1,15 @@
+import { styled } from "@mui/system";
+
+const SwitchInput = styled("input")`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  opacity: 0;
+  z-index: 1;
+  margin: 0;
+  cursor: pointer;
+`;
+
+export default SwitchInput;
