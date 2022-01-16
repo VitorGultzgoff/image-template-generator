c0974d527ccae9cd28c250321503d8ab6ebaea06
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2022-01-15 23:53:08 -0300

    refactor: reformatting the generate template file

--- src/components/GenerateTemplate/index.js
+++ src/components/GenerateTemplate/index.js
@@ -1,19 +1,21 @@
 // Libs
-import React, { useRef } from 'react'
-import ReactToPrint from 'react-to-print';
+import React, { useRef } from "react";
+import ReactToPrint from "react-to-print";
 
 // Components
-import Button from '@mui/material/Button'
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
 
 // Icons
-import PrintIcon from '@mui/icons-material/Print';
+import ImageIcon from "@mui/icons-material/Image";
+import PrintIcon from "@mui/icons-material/Print";
 
 // Utils
-import { formatBRLCurrency } from 'utils/currency'
+import { formatBRLCurrency } from "utils/currency";
 
 // Style
-import './index.css'
-import classNames from 'classnames';
+import "./index.css";
+import classNames from "classnames";
 
 function GenerateTemplate({ croppedPictures, picturesInfo }) {
   const componentRef = useRef();
@@ -35,24 +37,30 @@ function GenerateTemplate({ croppedPictures, picturesInfo }) {
       </div>
       <div id="contentToPrint" ref={componentRef}>
         {croppedPictures.map((actualPicture, actualPictureIndex) => {
-          const idValue = picturesInfo[actualPictureIndex].id
-          const amountValue = picturesInfo[actualPictureIndex].value
+          const idValue = picturesInfo[actualPictureIndex].id;
+          const amountValue = picturesInfo[actualPictureIndex].value;
           return (
             <div
-              className={
-                classNames("pictureContainer", { "noContentInBox": !idValue && !amountValue })
-              }
+              className={classNames("pictureContainer", {
+                noContentInBox: !idValue && !amountValue,
+              })}
             >
-              <img src={actualPicture} alt={actualPictureIndex} className="pictureImg" />
+              <img
+                src={actualPicture}
+                alt={actualPictureIndex}
+                className="pictureImg"
+              />
               {(idValue || amountValue) && (
-                <span className="valuesContainer">{idValue} {formatBRLCurrency(amountValue)}</span>
+                <span className="valuesContainer">
+                  {idValue} {formatBRLCurrency(amountValue)}
+                </span>
               )}
             </div>
-          )
+          );
         })}
       </div>
     </div>
-  )
+  );
 }
 
-export default GenerateTemplate
+export default GenerateTemplate;
