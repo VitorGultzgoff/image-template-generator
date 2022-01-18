f698697c46d8083c8a147c327b62c38a46eaadad
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2022-01-18 00:31:06 -0300

    refactor: redefining exportContentAsJPEG functionality based on the dependency

--- src/utils/image.js
+++ src/utils/image.js
@@ -1,13 +1,21 @@
+// Libs
+import { toJpeg} from 'html-to-image';
+
 const convertCanvasIntoImg = (canvasTarget) => {
   let imgConverted = new Image();
   imgConverted.src = canvasTarget.toDataURL();
   return imgConverted;
 };
 
-const exportContentAsImg = (contentTargetId) => {
-  const elementTarget = document.getElementById(contentTargetId)
-  // Convert, adapt the container and remove all the not include references
-  return elementTarget
+const exportContentAsJPEG = (contentTargetId, imageName = 'generated-image') => {
+  toJpeg(document.getElementById(contentTargetId), { quality: 0.95 })
+  .then(function (dataUrl) {
+    const fileType = 'jpeg'
+    let link = document.createElement('a');
+    link.download = `${imageName}.${fileType}`;
+    link.href = dataUrl;
+    link.click();
+  });
 }
 
-export { convertCanvasIntoImg, exportContentAsImg };
+export { convertCanvasIntoImg, exportContentAsJPEG };
