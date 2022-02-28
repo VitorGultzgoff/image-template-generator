87deea4b8ff3ee17f536a311597a7cb248e64738
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2022-02-28 08:54:11 -0300

    refactor: switching apptoolbar into a unique component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 // Components
-import AppBar from "@mui/material/AppBar";
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Toolbar from '@mui/material/Toolbar';
+import AppToolbar from "components/AppToolbar"
 
 // Containers
 import MainPage from "containers/MainPage";
@@ -10,34 +7,13 @@ import MainPage from "containers/MainPage";
 // Libs
 import "./i18n";
 
-// Icons
-import MenuBookIcon from '@mui/icons-material/MenuBook';
-import MenuIcon from '@mui/icons-material/Menu';
-
 // Styles
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <AppBar>
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          
-            <MenuBookIcon sx={{ mr: 1 }} />
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Image Template Generator
-            </Typography>
-        </Toolbar>
-      </AppBar>
+      <AppToolbar />
       <MainPage />
     </div>
   );
---
+++ src/components/AppToolbar/index.js
@@ -0,0 +1,35 @@
+// Components
+import AppBar from "@mui/material/AppBar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Toolbar from "@mui/material/Toolbar";
+
+// Libs
+import React from "react";
+
+// Icons
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import MenuIcon from "@mui/icons-material/Menu";
+
+export default function AppToolbar() {
+  return (
+    <AppBar>
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+
+        <MenuBookIcon sx={{ mr: 1 }} />
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Image Template Generator
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+}
