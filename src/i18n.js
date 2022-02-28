4c04c719c2035dcc30247577c89838be5426a74e
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2022-02-27 21:36:48 -0300

    chore: main definition of the i18n instance

---
+++ src/i18n.js
@@ -0,0 +1,32 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+const resources = {
+  en: {
+    translation: {
+      "image_template_generator": "Image template generator"
+    }
+  },
+  fr: {
+    translation: {
+      "image_template_generator": "Générateur de modèles d'images"
+    }
+  },
+  pt_br: {
+    translation: {
+      "image_template_generator": "Gerador de templates de imagem"
+    }
+  }
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: "en",
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+  export default i18n;
