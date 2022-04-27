df5048c435042575eef5a47361a46bbd9b626415
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2022-04-27 02:53:17 -0300

    refactor: switching from object structure into a json one in the translation module

--- src/i18n.js
+++ src/i18n.js
@@ -1,72 +1,15 @@
+// Libs import
 import i18n from "i18next";
 import detector from "i18next-browser-languagedetector"
 import { initReactI18next } from "react-i18next";
 
+// Translation resources
+import translationsEN from "translations/en/translations-en.json";
+import translationsPtBr from "translations/pt_br/translations-pt-br.json";
+
 const resources = {
-  en: {
-    translation: {
-      currency: {
-        brl: {
-          main_ticker: "R$"
-        }
-      },
-      functionalities: {
-        template_generation: "Template generation",
-        including_information: "Including information",
-        select_images: "Selection of images",
-        edit_images: "Edit images"
-      },
-      general: {
-        crop: "Crop",
-        nextImage: "Next",
-        previous: "Previous",
-        print: "Print",
-        product: {
-          identifier: "Product identifier",
-          value: "Product value"
-        }
-      },
-      image_template_generator: "Image template generator",
-      select_images: {
-        uploader: {
-          description: "Drag & Drop your files or",
-          complemented_description: "Search the file"
-        }
-      }
-    }
-  },
-  pt_br: {
-    translation: {
-      currency: {
-        brl: {
-          main_ticker: "R$"
-        }
-      },
-      functionalities: {
-        template_generation: "Geração do template",
-        including_information: "Inclusão de informações",
-        select_images: "Seleção de imagens",
-        edit_images: "Edição de imagens"
-      },
-      general: {
-        crop: "Recortar",
-        nextImage: "Próxima",
-        previous: "Anterior",
-        print: "Imprimir",
-        product: {
-          identifier: "Identificador do produto",
-          value: "Valor do produto"
-        }
-      },
-      image_template_generator: "Gerador de templates de imagem",
-      select_images: {
-        uploader: {
-          description: "Arraste e solte seus arquivos ou",
-          complemented_description: "Procure o arquivo"
-        }
-      }
-    },
-  }
+  en: { translation: translationsEN },
+  pt_br: { translation: translationsPtBr }
 };
 
 i18n
