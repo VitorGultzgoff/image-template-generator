5178f69e8a82f46553c5e69bc44d73276dc4db49
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2022-07-24 01:20:36 -0300

    refactor: switchforminput component switched into typescript definitions

---
+++ src/components/form/Switch/SwitchFormInput/index.tsx
@@ -0,0 +1,45 @@
+// Libs
+import React from "react";
+import clsx from "clsx";
+
+// Components
+import { useSwitch } from "@mui/base/SwitchUnstyled";
+import SwitchInput from "components/form/Switch/SwitchInput";
+import SwitchRoot from "components/form/Switch/SwitchRoot";
+import SwitchThumb from "components/form/Switch/SwitchThumb";
+import SwitchTrack from "components/form/Switch/SwitchTrack";
+import { OverridableComponent } from "@mui/material/OverridableComponent";
+import { SvgIconTypeMap } from "@mui/material";
+
+type SwitchFormInputProps = {
+  checked: boolean;
+  disabled: boolean;
+  focusVisible: boolean;
+  LeftIcon: OverridableComponent<SvgIconTypeMap<{}, "svg">>;
+  RightIcon: OverridableComponent<SvgIconTypeMap<{}, "svg">>;
+};
+export default function SwitchFormInput(props: SwitchFormInputProps) {
+  const { getInputProps, checked, disabled, focusVisible } = useSwitch(props);
+  const { LeftIcon, RightIcon } = props;
+
+  const stateClasses = {
+    checked,
+    disabled,
+    focusVisible,
+  };
+  return (
+    <SwitchRoot className={clsx(stateClasses)}>
+      <SwitchTrack>
+        <SwitchThumb className={clsx(stateClasses)}>
+          {LeftIcon && !checked && (
+            <LeftIcon className="colorWhite iconAsBackground" />
+          )}
+          {RightIcon && checked && (
+            <RightIcon className="colorWhite iconAsBackground" />
+          )}
+        </SwitchThumb>
+      </SwitchTrack>
+      <SwitchInput {...getInputProps()} aria-label="generic-switch-input" />
+    </SwitchRoot>
+  );
+}
