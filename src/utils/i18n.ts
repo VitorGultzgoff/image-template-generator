a2bb764fd4254687e7b26a00a2298a6b7bbad2ce
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2022-07-26 23:11:54 -0300

    refactor: i18n util switched into typescript definition

---
+++ src/utils/i18n.ts
@@ -0,0 +1,11 @@
+// Libs
+import i18n from "../i18n";
+
+// Utils
+import _isNil from "lodash/isNil";
+
+export const switchLanguage = (lang: string) => {
+  if (_isNil(lang)) return;
+  const { changeLanguage } = i18n;
+  changeLanguage(lang);
+};
