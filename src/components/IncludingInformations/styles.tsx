6edfd2674bb2803b01b7c9d391786004c8fe70e4
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2022-07-26 23:09:25 -0300

    refactor: including informations component switched into typescript definition

--- src/components/IncludingInformations/styles.tsx
+++ src/components/IncludingInformations/styles.tsx
@@ -1,32 +1,32 @@
-import Button from '@mui/material/Button'
-import Grid from '@mui/material/Grid'
-import styled from "styled-components"
-import TextField from "@mui/material/TextField"
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import styled from "styled-components";
+import TextField from "@mui/material/TextField";
 
 export const NextActionContainer = styled(Grid)`
   @media only screen and (min-width: 900px) {
     margin-top: 20px;
   }
-`
+`;
 
 export const IncludingInfoActionBtn = styled(Button)`
   min-width: 130px;
-`
+`;
 
 export const PreviousActionContainer = styled(Grid)`
   margin-top: 20px;
-`
+`;
 
 export const ProductInfoContainer = styled(Grid)`
   display: flex;
   margin-top: 25px;
   width: 100%;
-`
+`;
 
 export const ProductInfoInput = styled(TextField)`
   max-width: 350px;
-`
+`;
 
 export const ProductInfoInputValueContainer = styled(Grid)`
   margin-top: 10px;
-`
+`;
