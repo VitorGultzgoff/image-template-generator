76cea294f31bedbda5d424d9908cc4c78141e86f
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2022-07-26 23:11:45 -0300

    refactor: file util switched into typescript definition

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,30 +1,30 @@
 // Utils
 import _isEmpty from "lodash/isEmpty";
 
-const convertBase64IntoImg = (base64Data) => {
+const convertBase64IntoImg = (base64Data: string) => {
   if (_isEmpty(base64Data)) return base64Data;
   const img = new Image();
   img.src = base64Data;
-  img.style = "visibility: hidden;";
   document.body.appendChild(img);
   return img;
 };
 
-const exportDataAsJSON = (jsonData, dataName = 'generated-data') => {
-  if (_isEmpty(jsonData)) return false;
-  const dataStr = JSON.stringify(jsonData)
-  const dataUri = 'data:application/json;charset=utf-8,'+ encodeURIComponent(dataStr);
+const exportDataAsJSON = (jsonData: any, dataName = "generated-data") => {
+  if (_isEmpty(jsonData)) return;
+  const dataStr = JSON.stringify(jsonData);
+  const dataUri =
+    "data:application/json;charset=utf-8," + encodeURIComponent(dataStr);
   let link = document.createElement("a");
   link.download = `${dataName}-data.json`;
   link.href = dataUri;
   link.click();
 };
 
-const fileToDataURL = (file) => {
+const fileToDataURL = (file: File) => {
   var reader = new FileReader();
   return new Promise(function (resolve) {
     reader.onload = function (event) {
-      resolve(event.target.result);
+      resolve(event?.target?.result);
     };
     reader.readAsDataURL(file);
   });
