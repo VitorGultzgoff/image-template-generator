d1f55bed0d212c4255b1d57bb5f15ea522da35bf
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2022-07-26 23:09:46 -0300

    refactor: select images component switched into typescript definition

--- src/components/SelectImages/index.tsx
+++ src/components/SelectImages/index.tsx
@@ -3,7 +3,10 @@ import React from "react";
 import { FilePond, registerPlugin } from "react-filepond";
 
 // Libs
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
+
+// Models
+import { IPictureInformations } from "models/picture/picture.model";
 
 // Utils
 import { fileToDataURL } from "utils/file";
@@ -16,38 +19,47 @@ import "filepond/dist/filepond.min.css";
 import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
 import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+import { ActualFileObject, FilePondFile } from "filepond";
 registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
+
+type SelectImagesProps = {
+  croppedPictures: string[];
+  pictures: string[];
+  setCroppedPictures: (croppedPictures: string[]) => void;
+  setPictures: (pictures: string[]) => void;
+  setPicturesInfo: (picturesInfo: IPictureInformations[]) => void;
+};
 
 function SelectImages({
   pictures,
   setCroppedPictures,
   setPictures,
   setPicturesInfo,
-}) {
-  const { t } = useTranslation()
+}: SelectImagesProps) {
+  const { t } = useTranslation();
 
-  const setMappedPicturesInfo = (imgsData) => {
-    const picturesInfo = [];
+  const setMappedPicturesInfo = (imgsData: Blob[]) => {
+    const picturesInfo: IPictureInformations[] = [];
     // May be replaced for an array create without forEach
     imgsData.forEach((actualImg) => {
       picturesInfo.push({
-        id: "",
-        value: "",
+        id: 0,
+        value: 0,
       });
     });
     setPicturesInfo(picturesInfo);
   };
 
-  const transformFilesIntoImages = (files) => {
+  const transformFilesIntoImages = (files: ActualFileObject[]) => {
     const filesArray = Array.prototype.slice.call(files);
     return Promise.all(filesArray.map(fileToDataURL));
   };
 
-  const transformPondDataIntoImages = (pondData) => {
+  const transformPondDataIntoImages = (pondData: FilePondFile[]) => {
     return pondData.map((actualPondData) => actualPondData.file);
   };
 
-  const uploadImages = (pondData, quantityOfImagesUploaded) => {
+  const uploadImages = (pondData: FilePondFile[]) => {
     const files = transformPondDataIntoImages(pondData);
     const images = transformFilesIntoImages(files);
     images.then((imgsData) => {
@@ -66,7 +78,11 @@ function SelectImages({
         allowMultiple={true}
         maxFiles={250}
         name="files"
-        labelIdle={`${t('select_images.uploader.description')} <span class="filepond--label-action">${t('select_images.uploader.description')}</span>`}
+        labelIdle={`${t(
+          "select_images.uploader.description"
+        )} <span class="filepond--label-action">${t(
+          "select_images.uploader.description"
+        )}</span>`}
       />
     </>
   );
