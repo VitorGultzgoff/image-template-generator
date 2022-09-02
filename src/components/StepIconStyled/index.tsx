92dd1b19baf36b246b1ce43eb86f5b94b9f7fcac
Vitor Gultzgoff <vitor@fyclabs.com>
2022-09-02 00:28:11 -0300

    style: material ui v5 updates on stepper component styles

--- src/components/StepIconStyled/index.tsx
+++ src/components/StepIconStyled/index.tsx
@@ -11,30 +11,32 @@ import WorkIcon from "@mui/icons-material/Work";
 import { StepIconProps, styled } from "@mui/material";
 import { isNil } from "lodash";
 
-const StyledContainer = styled("div")({
-  root: {
-    backgroundColor: "#ccc",
-    zIndex: 1,
-    color: "#fff",
-    width: 50,
-    height: 50,
-    display: "flex",
-    borderRadius: "50%",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  active: {
+const StyledContainer = styled("div")<{
+  ownerState: { completed?: boolean; active?: boolean };
+}>(({ theme, ownerState }) => ({
+  backgroundColor:
+    theme.palette.mode === "dark" ? theme.palette.grey[700] : "#ccc",
+  zIndex: 1,
+  color: "#fff",
+  width: 50,
+  height: 50,
+  display: "flex",
+  borderRadius: "50%",
+  justifyContent: "center",
+  alignItems: "center",
+  ...(ownerState.active && {
     backgroundImage:
       "linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)",
     boxShadow: "0 4px 10px 0 rgba(0,0,0,.25)",
-  },
-  completed: {
+  }),
+  ...(ownerState.completed && {
     backgroundImage:
       "linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)",
-  },
-});
+  }),
+}));
 
 function StepIconStyled(props: StepIconProps) {
+  const { active, completed, className } = props;
   const icons = {
     1: <PanToolIcon />,
     2: <CropIcon />,
@@ -42,11 +44,19 @@ function StepIconStyled(props: StepIconProps) {
     4: <WorkIcon />,
   };
 
-  if (isNil(props?.icon)) return <StyledContainer />;
+  if (isNil(props?.icon))
+    return (
+      <StyledContainer
+        ownerState={{ completed, active }}
+        className={className}
+      />
+    );
   const iconStr = parseInt(props?.icon.toString());
 
   return (
-    <StyledContainer>{icons[iconStr as keyof typeof icons]}</StyledContainer>
+    <StyledContainer ownerState={{ completed, active }} className={className}>
+      {icons[iconStr as keyof typeof icons]}
+    </StyledContainer>
   );
 }
 
