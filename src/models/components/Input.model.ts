fa4fb35d2f59c31ca36171a66d960896594d30e8
Vitor Gultzgoff <vitor@fyclabs.com>
2022-09-02 00:27:20 -0300

    fix: value change mapper for currency input component

--- src/models/components/Input.model.ts
+++ src/models/components/Input.model.ts
@@ -1,6 +1,8 @@
+// Libs imports
 import React from "react";
 import { NumberFormatProps } from "react-number-format";
 
 export interface ICurrencyInput {
   component: React.ComponentClass<NumberFormatProps>;
+  setValue: (value: number | undefined) => void;
 }
